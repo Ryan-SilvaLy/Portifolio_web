fc135e0c77e452e084705e567ef56daaed641267
Your Name <you@example.com>
2026-05-02 18:27:54 -0300

    Criando estrutura e baixando estensoes

---
+++ Frontend/src/app/components/projects-carousel/projects-carousel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-carousel',
+  imports: [],
+  templateUrl: './projects-carousel.html',
+  styleUrl: './projects-carousel.scss',
+})
+export class ProjectsCarousel {}
